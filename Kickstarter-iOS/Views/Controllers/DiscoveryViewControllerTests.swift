19567ec20ebd9652950acb4c2ac889556b44a505
Pavel Dusatko <pavel.dusatko@gmail.com>
2018-11-16 23:49:10 -0800

    Fix the tests

--- Kickstarter-iOS/Views/Controllers/DiscoveryViewControllerTests.swift
+++ Kickstarter-iOS/Views/Controllers/DiscoveryViewControllerTests.swift
@@ -8,14 +8,15 @@ internal final class DiscoveryViewControllerTests: TestCase {
 
     let configureExpectation = expectation(description: "Waiting for view controllers to be configured")
 
-    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    var pageViewController: UIPageViewController?
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+      while pageViewController?.viewControllers?.first?.view as? UIScrollView == nil {
+        pageViewController = vc.children.compactMap { $0 as? UIPageViewController }.first
+      }
       configureExpectation.fulfill()
     }
-
-    waitForExpectations(timeout: 5, handler: nil)
-
-    let pageViewController = vc.children
-      .compactMap { $0 as? UIPageViewController }.first
+    waitForExpectations(timeout: 10, handler: nil)
 
     XCTAssertNotNil(pageViewController?.viewControllers?.first?.view as? UIScrollView)
   }
